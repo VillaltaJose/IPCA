e8c183ebf995a2bd1fe0765be87d92b0c2fa81ee
Jose Villalta <josemiguelvillaltah@gmail.com>
2020-02-04 16:00:30 -0500

    Pruebas Conexion y Consultas

---
+++ API/src/controllers/index.controller.js
@@ -0,0 +1,48 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+	host: 'localhost',
+	user: 'postgres',
+	password: 'root',
+	database: 'firstapi',
+	port: '5432'
+})
+
+
+const getUsers = async (req, res) => {
+	const response = await pool.query("SELECT * FROM users");
+	res.status(200).json(response.rows);
+}
+
+const setUser = async (req, res) => {
+	const { name, email } = req.body;
+
+	const response = await pool.query("INSERT INTO users(name, email) VALUES ($1, $2)", [name, email]);
+	console.log(response);
+	res.json({
+		message: "Usuario creado correctamente",
+		body: {
+			user: {name, email}
+		}
+	});
+}
+
+const getUserById = async (req, res) => {
+	const response = await pool.query("SELECT * FROM users WHERE id=$1", [req.params.id]);
+	console.log(response);
+	res.json(response.rows);
+}
+
+const delUserById = async (req, res) => {
+	const response = await pool.query("DELETE FROM users WHERE id=$1", [req.params.id]);
+	console.log(response);
+	res.json(`User ${req.params.id} eliminado correctamente!`);
+}
+
+
+module.exports = {
+	getUsers,
+	setUser,
+	getUserById,
+	delUserById
+}
--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -1,8 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 
+const { getUsers, setUser, getUserById, delUserById } = require('../controllers/index.controller');
+
 module.exports = router;
 
-router.get('/users', (req, res) => {
-	res.send("Users");
-});
+router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
+
+router.post('/users', setUser);
+
+router.delete('/users/:id', delUserById);
